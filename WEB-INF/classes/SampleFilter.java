789831519b580b036c17647a60772d0074aff43d
Suzukiti-1029 <h40396suzujun@gmail.com>
2022-03-21 11:24:16 +0900

    フィルタ

---
+++ WEB-INF/classes/SampleFilter.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+
+@WebFilter("/*")
+public class SampleFilter implements Filter {
+  public void doFilter(ServletRequest request,
+  ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    request.setCharacterEncoding("UTF-8");
+    response.setContentType("text/html; charset=UTF-8");
+
+    chain.doFilter(request, response);
+  }
+}
